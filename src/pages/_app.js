7fdb1b246b64dd9e0fb7bb02f58491edb0f51bd0
alexei88 <alexeidz81@gmail.com>
2023-04-19 22:32:16 +0530

    Created research front end

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,43 @@
-import '@/styles/globals.css'
+import Head from 'next/head';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { useNProgress } from 'src/hooks/use-nprogress';
+import { createTheme } from 'src/theme';
+import 'simplebar-react/dist/simplebar.min.css';
+import { Layout } from '../layouts/dashboard/layout';
+import { useRouter } from 'next/router';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+const App = (props) => {
+  const { Component, pageProps } = props;
+  useNProgress();
+
+  const router = useRouter();
+
+  const theme = createTheme();
+  if(router.pathname === '/404') return <Component {...pageProps} />;
+  if(router.pathname === '/') return <Component {...pageProps} />;
+  if(router.pathname === '/register') return <Component {...pageProps} />;
+  return (
+    <div >
+      <Head>
+        <title>
+          Dashboard
+        </title>
+        <meta
+          name="viewport"
+          content="initial-scale=1, width=device-width"
+        />
+      </Head>
+
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+           <Layout>
+             <Component {...pageProps} />
+           </Layout>
+          </ThemeProvider>
+    </div>
+  );
+};
+
+export default App;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="shortcut icon" href="/icon.png" />
+        </Head>
+        <body>
         <Main />
         <NextScript />
-      </body>
-    </Html>
-  )
+        </body>
+      </Html>
+    );
+  }
 }
+
+
+export default CustomDocument;
